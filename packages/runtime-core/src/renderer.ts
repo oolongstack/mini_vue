8e0241f42c06c04431bbbaefb5cdf1a5ce8f1834
dduohereshui <2245675739@qq.com>
2022-08-16 23:34:19 +0800

    feat: 基本的diff算法完成

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -21,10 +21,12 @@ export function createRenderer(renderOptions) {
     for (let i = 0; i < children.length; i++) {
       const child = normalize(children[i]);
       children[i] = child; // 字符串替换为vnode
+      console.log(child);
+
       patch(null, child, container);
     }
   };
-  const mountElement = (vnode, container) => {
+  const mountElement = (vnode, container, anchor) => {
     const { type, props, children, shapeFlag } = vnode;
     // 创建元素
     const el = (vnode.el = hostCreateElement(type));
@@ -42,12 +44,12 @@ export function createRenderer(renderOptions) {
         mountChildren(children, el);
       }
     }
-    hostInsert(el, container);
+    hostInsert(el, container, anchor);
   };
-  const processElement = (n1, n2, container) => {
+  const processElement = (n1, n2, container, anchor) => {
     if (n1 == null) {
       // 挂载
-      mountElement(n2, container);
+      mountElement(n2, container, anchor);
     } else {
       // 复用老节点，不用重新挂载到container上
       patchElement(n1, n2);
@@ -56,9 +58,12 @@ export function createRenderer(renderOptions) {
   const patchElement = (n1, n2) => {
     // 先复用节点，再比对属性，最后比对儿子节点
     const el = (n2.el = n1.el);
+
     const oldProps = n1.props || {};
     const newProps = n2.props || {};
+
     patchProps(oldProps, newProps, el);
+
     patchChildren(n1, n2, el);
   };
   const patchProps = (oldProps, newProps, el) => {
@@ -104,6 +109,7 @@ export function createRenderer(renderOptions) {
       if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) {
         if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
           // diff算法
+          patchKeyedChildren(c1, c2, el);
         } else {
           // 之前是数组，现在是文本，删除老的数组，挂载新的文本节点
           unmountChildren(c1);
@@ -119,10 +125,100 @@ export function createRenderer(renderOptions) {
       }
     }
   };
-  const processText = (n1, n2, container) => {
+  const patchKeyedChildren = (c1, c2, el) => {
+    let i = 0;
+    let e1 = c1.length - 1;
+    let e2 = c2.length - 1;
+
+    // sync from start
+    while (i <= e1 && i <= e2) {
+      const n1 = c1[i];
+      const n2 = c2[i];
+      if (isSameVnode(n1, n2)) {
+        patch(n1, n2, el);
+      } else {
+        break;
+      }
+      i++;
+    }
+    // sync from end
+    while (i <= e1 && i <= e2) {
+      const n1 = c1[e1];
+      const n2 = c2[e2];
+      if (isSameVnode(n1, n2)) {
+        patch(n1, n2, el);
+      } else {
+        break;
+      }
+      e1--;
+      e2--;
+    }
+    // common sequence + mount     i > e1 && i <= e2  挂载i到e2
+    if (i > e1) {
+      if (i <= e2) {
+        while (i <= e2) {
+          const nextPos = e2 + 1;
+          const anchor = nextPos < c2.length ? c2[nextPos].el : null;
+          patch(null, c2[i], el, anchor);
+          i++;
+        }
+      }
+    }
+    // common sequence + unmount   i <= e1 && i > e2  删除i到e1
+    else if (i > e2) {
+      if (i <= e1) {
+        while (i <= e1) {
+          unmount(c1[i]);
+          i++;
+        }
+      }
+    }
+
+    // 乱序比对
+    console.log(i, e1, e2);
+    let s1 = i;
+    let s2 = i;
+    // 新的乱序序列的key对应的索引
+    const keyToNewIndexMap = new Map();
+    for (let i = s2; i <= e2; i++) {
+      const childVnode = c2[i];
+      keyToNewIndexMap.set(childVnode.key, i);
+    }
+
+    console.log(keyToNewIndexMap);
+    const toBePatched = e2 - s2 + 1;
+    const newIndexToOldIndexMap = new Array(toBePatched).fill(0);
+    // 区老列表里去找key相同的进行patch，找不到的就删除
+    for (let i = s1; i <= e1; i++) {
+      const oldChild = c1[i];
+      const newIndex = keyToNewIndexMap.get(oldChild.key); // 寻找新的节点中是否有老的节点的key
+      if (!newIndex) {
+        unmount(oldChild);
+      } else {
+        newIndexToOldIndexMap[newIndex - s2] = i + 1;
+        patch(oldChild, c2[newIndex], el);
+      }
+    }
+
+    // patch完之后倒序插入
+    for (let i = toBePatched - 1; i >= 0; i--) {
+      const index = i + s2;
+      const current = c2[index];
+      const anchor = index + 1 < c2.length ? c2[index + 1].el : null;
+
+      if (newIndexToOldIndexMap[i] === 0) {
+        // 需要创建
+        patch(null, current, el, anchor);
+      } else {
+        // 需要移动
+        hostInsert(current.el, el, anchor);
+      }
+    }
+  };
+  const processText = (n1, n2, container, anchor) => {
     if (n1 == null) {
       const el = (n2.el = hostCreateText(n2.children));
-      hostInsert(el, container);
+      hostInsert(el, container, anchor);
     } else {
       const el = (n2.el = n1.el);
       if (n1.children !== n2.children) {
@@ -131,7 +227,7 @@ export function createRenderer(renderOptions) {
     }
   };
   // 渲染器核心
-  const patch = (n1, n2, container) => {
+  const patch = (n1, n2, container, anchor = null) => {
     if (n1 === n2) return;
     // 若不是同样的虚拟节点  (n1必须存在，不然就是挂载)
     if (n1 && !isSameVnode(n1, n2)) {
@@ -142,11 +238,11 @@ export function createRenderer(renderOptions) {
     const { type, shapeFlag } = n2;
     switch (type) {
       case Text:
-        processText(n1, n2, container);
+        processText(n1, n2, container, anchor);
         break;
       default:
         if (shapeFlag & ShapeFlags.ELEMENT) {
-          processElement(n1, n2, container);
+          processElement(n1, n2, container, anchor);
         }
     }
   };
--- packages/runtime-dom/dist/runtime-dom.global.js
+++ packages/runtime-dom/dist/runtime-dom.global.js
@@ -204,10 +204,11 @@ var VueRuntimeDOM = (() => {
       for (let i = 0; i < children.length; i++) {
         const child = normalize(children[i]);
         children[i] = child;
+        console.log(child);
         patch(null, child, container);
       }
     };
-    const mountElement = (vnode, container) => {
+    const mountElement = (vnode, container, anchor) => {
       const { type, props, children, shapeFlag } = vnode;
       const el = vnode.el = hostCreateElement(type);
       if (props) {
@@ -222,11 +223,11 @@ var VueRuntimeDOM = (() => {
           mountChildren(children, el);
         }
       }
-      hostInsert(el, container);
+      hostInsert(el, container, anchor);
     };
-    const processElement = (n1, n2, container) => {
+    const processElement = (n1, n2, container, anchor) => {
       if (n1 == null) {
-        mountElement(n2, container);
+        mountElement(n2, container, anchor);
       } else {
         patchElement(n1, n2);
       }
@@ -269,6 +270,7 @@ var VueRuntimeDOM = (() => {
       } else {
         if (prevShapeFlag & 16 /* ARRAY_CHILDREN */) {
           if (shapeFlag & 16 /* ARRAY_CHILDREN */) {
+            patchKeyedChildren(c1, c2, el);
           } else {
             unmountChildren(c1);
           }
@@ -282,10 +284,84 @@ var VueRuntimeDOM = (() => {
         }
       }
     };
-    const processText = (n1, n2, container) => {
+    const patchKeyedChildren = (c1, c2, el) => {
+      let i = 0;
+      let e1 = c1.length - 1;
+      let e2 = c2.length - 1;
+      while (i <= e1 && i <= e2) {
+        const n1 = c1[i];
+        const n2 = c2[i];
+        if (isSameVnode(n1, n2)) {
+          patch(n1, n2, el);
+        } else {
+          break;
+        }
+        i++;
+      }
+      while (i <= e1 && i <= e2) {
+        const n1 = c1[e1];
+        const n2 = c2[e2];
+        if (isSameVnode(n1, n2)) {
+          patch(n1, n2, el);
+        } else {
+          break;
+        }
+        e1--;
+        e2--;
+      }
+      if (i > e1) {
+        if (i <= e2) {
+          while (i <= e2) {
+            const nextPos = e2 + 1;
+            const anchor = nextPos < c2.length ? c2[nextPos].el : null;
+            patch(null, c2[i], el, anchor);
+            i++;
+          }
+        }
+      } else if (i > e2) {
+        if (i <= e1) {
+          while (i <= e1) {
+            unmount(c1[i]);
+            i++;
+          }
+        }
+      }
+      console.log(i, e1, e2);
+      let s1 = i;
+      let s2 = i;
+      const keyToNewIndexMap = /* @__PURE__ */ new Map();
+      for (let i2 = s2; i2 <= e2; i2++) {
+        const childVnode = c2[i2];
+        keyToNewIndexMap.set(childVnode.key, i2);
+      }
+      console.log(keyToNewIndexMap);
+      const toBePatched = e2 - s2 + 1;
+      const newIndexToOldIndexMap = new Array(toBePatched).fill(0);
+      for (let i2 = s1; i2 <= e1; i2++) {
+        const oldChild = c1[i2];
+        const newIndex = keyToNewIndexMap.get(oldChild.key);
+        if (!newIndex) {
+          unmount(oldChild);
+        } else {
+          newIndexToOldIndexMap[newIndex - s2] = i2 + 1;
+          patch(oldChild, c2[newIndex], el);
+        }
+      }
+      for (let i2 = toBePatched - 1; i2 >= 0; i2--) {
+        const index = i2 + s2;
+        const current = c2[index];
+        const anchor = index + 1 < c2.length ? c2[index + 1].el : null;
+        if (newIndexToOldIndexMap[i2] === 0) {
+          patch(null, current, el, anchor);
+        } else {
+          hostInsert(current.el, el, anchor);
+        }
+      }
+    };
+    const processText = (n1, n2, container, anchor) => {
       if (n1 == null) {
         const el = n2.el = hostCreateText(n2.children);
-        hostInsert(el, container);
+        hostInsert(el, container, anchor);
       } else {
         const el = n2.el = n1.el;
         if (n1.children !== n2.children) {
@@ -293,7 +369,7 @@ var VueRuntimeDOM = (() => {
         }
       }
     };
-    const patch = (n1, n2, container) => {
+    const patch = (n1, n2, container, anchor = null) => {
       if (n1 === n2)
         return;
       if (n1 && !isSameVnode(n1, n2)) {
@@ -303,11 +379,11 @@ var VueRuntimeDOM = (() => {
       const { type, shapeFlag } = n2;
       switch (type) {
         case Text:
-          processText(n1, n2, container);
+          processText(n1, n2, container, anchor);
           break;
         default:
           if (shapeFlag & 1 /* ELEMENT */) {
-            processElement(n1, n2, container);
+            processElement(n1, n2, container, anchor);
           }
       }
     };
